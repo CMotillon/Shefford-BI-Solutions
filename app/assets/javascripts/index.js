1153807b4f0d8f85e77cfdb865cc5a1b1a688a50
CMotillon <cedricmotillon@outlook.com>
2021-05-18 17:38:00 -0400

    update

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -30,6 +30,84 @@ $(document).ready(function(){
         $("#contact").hide();
         $("#realisations").hide();
         $("#slogan").hide();
+
+        $("#detail1").hide();
+        $("#detail2").hide();
+        $("#detail3").hide();
+        $("#detail4").hide();
+        $("#detail5").hide();
+        $("#detail6").hide();
+        $("#detail7").hide();
+
+        $("#detail1_clicked").click(function(){
+            $("#detail1").show();
+            $("#detail2").hide();
+            $("#detail3").hide();
+            $("#detail4").hide();
+            $("#detail5").hide();
+            $("#detail6").hide();
+            $("#detail7").hide();
+        });
+        
+        $("#detail2_clicked").click(function(){
+            $("#detail1").hide();
+            $("#detail2").show();
+            $("#detail3").hide();
+            $("#detail4").hide();
+            $("#detail5").hide();
+            $("#detail6").hide();
+            $("#detail7").hide();
+        });
+        
+        $("#detail3_clicked").click(function(){
+            $("#detail1").hide();
+            $("#detail2").hide();
+            $("#detail3").show();
+            $("#detail4").hide();
+            $("#detail5").hide();
+            $("#detail6").hide();
+            $("#detail7").hide();
+        });
+        
+        $("#detail4_clicked").click(function(){
+            $("#detail1").hide();
+            $("#detail2").hide();
+            $("#detail3").hide();
+            $("#detail4").show();
+            $("#detail5").hide();
+            $("#detail6").hide();
+            $("#detail7").hide();
+        });
+        
+        $("#detail5_clicked").click(function(){
+            $("#detail1").hide();
+            $("#detail2").hide();
+            $("#detail3").hide();
+            $("#detail4").hide();
+            $("#detail5").show();
+            $("#detail6").hide();
+            $("#detail7").hide();
+        });
+        
+        $("#detail6_clicked").click(function(){
+            $("#detail1").hide();
+            $("#detail2").hide();
+            $("#detail3").hide();
+            $("#detail4").hide();
+            $("#detail5").hide();
+            $("#detail6").show();
+            $("#detail7").hide();
+        });
+        
+        $("#detail7_clicked").click(function(){
+            $("#detail1").hide();
+            $("#detail2").hide();
+            $("#detail3").hide();
+            $("#detail4").hide();
+            $("#detail5").hide();
+            $("#detail6").hide();
+            $("#detail7").show();
+        });
     });
     
     $("#blog_clicked").click(function(){
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ApplicationMailer
     def contact_form(contact)
         @contact = contact
         email_subject = 'Demande de contact par ' + contact.name
-        email_body = contact.description + "\nEmail: " + contact.email + "\nTelephone: " + contact.phone
-        
+        email_body = contact.description + "\n \nEmail: " + contact.email + "\nTelephone: " + contact.phone
+
         mail(to: 'cedricmotillon@outlook.com', subject: email_subject, body: email_body)
     end
 end
